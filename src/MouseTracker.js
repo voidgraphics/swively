311d0fbbf571d2f913e43645d278ef458f031f2d
Adrien Leloup <adrien@whitecube.be>
2018-11-06 23:29:36 +0100

    Added setTransformOrigin & fixed indentation

--- src/MouseTracker.js
+++ src/MouseTracker.js
@@ -1,19 +1,19 @@
 export default class MouseTracker {
-  
-  constructor() {
-    this._x = 0;
-    this._y = 0;
-    this.x = 0;
-    this.y = 0;
-  }
-  
-  updatePosition(e) {
-    this.x = e.clientX - this._x;
-    this.y = (e.clientY - this._y) * -1;
-  }
-  
-  setOrigin(container) {
-    this._x = container.offsetLeft + Math.floor(container.offsetWidth / 2);
-    this._y = container.offsetTop + Math.floor(container.offsetHeight / 2);
-  }
+
+    constructor() {
+        this._x = 0;
+        this._y = 0;
+        this.x = 0;
+        this.y = 0;
+    }
+
+    updatePosition(e) {
+        this.x = e.clientX - this._x;
+        this.y = (e.clientY - this._y) * -1;
+    }
+
+    setOrigin(container) {
+        this._x = container.offsetLeft + Math.floor(container.offsetWidth / 2);
+        this._y = container.offsetTop + Math.floor(container.offsetHeight / 2);
+    }
 }
--- src/Swively.js
+++ src/Swively.js
@@ -1,45 +1,50 @@
 import MouseTracker from './MouseTracker.js';
 
-export default class Swivel {
-  
-  constructor(container, inner, options) {
-    this.mouse = new MouseTracker;
-    this.container = container;
-    this.inner = inner;
-    this.mouse.setOrigin(this.container);
-    this.setPerspective(options.perspective);
-    this.setTransition(options.transition);
-    this.setEvents();
-  }
-  
-  setEvents() {
-    this.container.addEventListener('mouseenter', (e) => this.update(e));
-    this.container.addEventListener('mouseleave', (e) => this.reset());
-    this.container.addEventListener('mousemove', (e) => this.update(e));
-  }
-  
-  setPerspective(amount) {
-    this.container.style.perspective = amount + 'px';
-  }
-  
-  setTransition(duration) {
-    this.inner.style.transition = 'transform ' + duration + 'ms';
-  }
-  
-  update(e) {
-    this.mouse.updatePosition(e);
-    let x = (this.mouse.y / inner.offsetHeight / 2).toFixed(2);
-    let y = (this.mouse.x / inner.offsetWidth / 2).toFixed(2);
-    this.setTransform(x, y);
-  }
-  
-  setTransform(x, y) {
-    var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
-    inner.style.transform = style;
-  }
-  
-  reset() {
-    this.inner.style.transform = null;
-  }
-  
+export default class Swively {
+
+    constructor(container, inner, options) {
+        this.mouse = new MouseTracker;
+        this.container = container;
+        this.inner = inner;
+        this.mouse.setOrigin(this.container);
+        this.setPerspective(options.perspective);
+        this.setTransition(options.transition);
+        this.setTransformOrigin();
+        this.setEvents();
+    }
+
+    setEvents() {
+        this.container.addEventListener('mouseenter', (e) => this.update(e));
+        this.container.addEventListener('mouseleave', (e) => this.reset());
+        this.container.addEventListener('mousemove', (e) => this.update(e));
+    }
+
+    setPerspective(amount) {
+        this.container.style.perspective = amount + 'px';
+    }
+
+    setTransformOrigin(position = 'center center') {
+        this.inner.style.transformOrigin = position;
+    }
+
+    setTransition(duration) {
+        this.inner.style.transition = 'transform ' + duration + 'ms';
+    }
+
+    update(e) {
+        this.mouse.updatePosition(e);
+        let x = (this.mouse.y / inner.offsetHeight / 2).toFixed(2);
+        let y = (this.mouse.x / inner.offsetWidth / 2).toFixed(2);
+        this.setTransform(x, y);
+    }
+
+    setTransform(x, y) {
+        var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+        inner.style.transform = style;
+    }
+
+    reset() {
+        this.inner.style.transform = null;
+    }
+
 }
